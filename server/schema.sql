b085d983b84fc30644588e0667aea6f7f918d3b1
Eric King <e.king117@gmail.com>
2015-07-24 17:57:39 -0400

    Schema updated, needs to be tested

--- server/schema.sql
+++ server/schema.sql
@@ -3,15 +3,34 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  /* MESSAGES TABLE
+   * id
+   * text
+   * creator
+   * date created
+   * room
+  */
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  text VARCHAR(),
+  creator VARCHAR(),
+  date DATE,
+  room VARCHAR()
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE USERS (
+  /* USERS TABLE
+   * userid (do we have to make this?)
+   * username
+  */
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR()
+);
 
-
+  /* BUDDIES TABLE (2-column table) (EC?)
+   * users that are associated with each other
+  */
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+ *  to create the database and the tables.*
